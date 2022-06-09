860a209ad830fc7d14b6eb4c75477813a8b31daa
Elbek Khamraev <xamraevelbek14@gmail.com>
2022-06-09 01:04:38 -0400

    Initial Commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import React,{useEffect,useState} from 'react';
-import Topbar from './Topbar';
-import { useAuth } from './context/AuthContext';
+import Topbar from './Topbar.js';
+import { useAuth } from './context/AuthContext.js';
 export default function Layout(props){
     const {user}= useAuth();
     return (
